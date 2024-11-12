279f047ea7a19bf400176550a140db75c6a74f22
Juan Miguel Garcia Vargas <mvyt2021@gmail.com>
2024-11-11 20:26:07 -0600

    pizzas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddMessageComponent } from "./tem/add-message/add-message.component";
 import { ListMessageComponent } from "./tem/list-message/list-message.component";
+import { CapturaPedidoComponent } from "./formulario/pizzas/captura-pedido/captura-pedido.component";
+import { DetallePedidoComponent } from "./formulario/pizzas/detalle-pedido/detalle-pedido.component";
+import { VentasDiaComponent } from "./formulario/pizzas/ventas-dia/ventas-dia.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddMessageComponent, ListMessageComponent],
+  imports: [RouterOutlet, AddMessageComponent, ListMessageComponent, CapturaPedidoComponent, DetallePedidoComponent, VentasDiaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
 })
